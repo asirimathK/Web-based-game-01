6ceb5c6db122b0e0b1ce0fcbf299a21274c79628
Asirimath <asirimath20@gmail.com>
2022-02-05 19:18:08 +0530

    Player movement

--- index.js
+++ index.js
@@ -14,29 +14,81 @@ class Player {
         }
         this.velocity ={
             x: 0,
-            y: 1
+            y: 0
         }
 
         this.width = 30
         this.height = 30
     }
     draw() {
-        c.fillStyle='red'
+        c.fillStyle='darkcyan'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        if(this.position.y +this.height +this.velocity.y <= canvas.height)
         this.velocity.y += gravity
+        else this.velocity.y = 0
         
     }
 }
 const player = new Player()
+const keys = {
+    right: {pressed: false },
+    left: {pressed: false}
+}
 
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height )
     player.update()
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    }
+    else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } 
+    else player.velocity.x = 0
 }
 animate()
+
+window.addEventListener('keydown', ({ keyCode })=> {console.log(keyCode)
+    
+     switch (keyCode){ 
+        case 65: console.log('left')
+        keys.left.pressed = true
+        break
+        case 83: console.log('down') 
+        break
+        case 87: console.log('up') 
+        player.velocity.y -= 10
+        break
+        case 68: console.log('right') 
+        keys.right.pressed = true
+        break
+    } 
+    console.log(keys.right.pressed)
+    })
+    window.addEventListener('keyup', ({ keyCode })=> {console.log(keyCode)
+    
+        switch (keyCode){ 
+           case 65: console.log('left')
+           keys.left.pressed = false
+           break
+           case 83: console.log('down') 
+           break
+           case 87: console.log('up') 
+           
+           break
+           case 68: console.log('right') 
+           keys.right.pressed = false
+           break
+       } 
+       console.log(keys.right.pressed)
+       })
+
